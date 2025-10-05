4f4ba6838d3a3d6b3db4606c49eea25b7caad8a4
Eric-Manzanilla <ericmanzanilla@outlook.com>
2025-10-04 18:12:05 -0600

    uPDATING

---
+++ src/main/java/Dropdown.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Dropdown {
+    public static void main (String[] args) throws InterruptedException{
+
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        // //a[@value='MAA'] - xPath for Chennai
+        // //a[@value='BLR'] - xPath for Bangalore
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("ctl00_mainContent_ddl_originStation1_CTXT")));
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@value='BLR']")));
+        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[@value='MAA'])[2]")));
+        driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+
+
+
+    }
+}
