5fc2d6ba322badb39e0382b4469881ba75464c88
ericmanzanillac <ericmanzanilla@outlook.com>
2025-10-10 13:22:39 -0600

    Updating with more classes from Rahul course

--- src/main/java/fluentWait.java
+++ src/main/java/fluentWait.java
@@ -5,11 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
-import org.w3c.dom.Text;
-
-import java.util.function.Function;
 import java.time.Duration;
-import java.util.function.Function;
+
 
 public class fluentWait {
     public static void main(String[] args) {
@@ -22,13 +19,11 @@ public class fluentWait {
                 pollingEvery(Duration.ofSeconds(3)).ignoring(NoSuchElementException.class);
 //en fluent wait no hay customized cosas para tu wait
 
-        WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
-            public WebElement apply(WebDriver driver) {
-                if (driver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed()) {
-                    return driver.findElement(By.cssSelector("[id='finish'] h4"));
-                } else
-                    return null;
-            }
+        WebElement foo = wait.until(driver1 -> {
+            if (driver1.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed()) {
+                return driver1.findElement(By.cssSelector("[id='finish'] h4"));
+            } else
+                return null;
         });
         System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).getText());
     }
