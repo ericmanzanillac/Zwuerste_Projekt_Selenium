a298356142e8318ac1a00b3ab79497adbdba21b1
ericmanzanillac <ericmanzanilla@outlook.com>
2025-10-16 00:00:41 -0600

    Updates from Section 10

--- src/main/java/scope.java
+++ src/main/java/scope.java
@@ -4,8 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class scope {
-    public static void main(String[]args){
+    public static void main(String[]args) throws InterruptedException {
 
         //Give me the count of links on the page
         WebDriver driver = new ChromeDriver();
@@ -20,13 +23,22 @@ public class scope {
         System.out.println(macBook.findElements(By.tagName("a")).size());
 
         //click each link in the column and check if its opening
-        for(int i = 0; i<macBook.findElements(By.tagName("a")).size(); i++){
-            
-            String clickonlinkTab = Keys.chord(Keys.CONTROL, Keys.ENTER);
+        for(int i = 1; i<macBook.findElements(By.tagName("a")).size(); i++) {
 
-            macBook.findElements(By.tagName("a")).get(i).sendKeys(clickonlinkTab);
+            String onclickTab = Keys.chord(Keys.CONTROL, Keys.ENTER);
+            macBook.findElements(By.tagName("a")).get(i).sendKeys(onclickTab);
+            Thread.sleep(5000L);
+        }
 
+        Set<String> tabsIte = driver.getWindowHandles();
+        Iterator<String> iterator = tabsIte.iterator();
+
+        while(iterator.hasNext()){
+                driver.switchTo().window(iterator.next());
+                System.out.println(driver.getTitle());
+            }
 
         }
+
     }
-}
+
